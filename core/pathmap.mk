c963c9bec6c203a0df826a1c4c142b415773e2b8
klozz <klozz707@gmail.com>
2017-08-31 00:27:49 -0400

    fix some paths part2
    Change-Id: Ic6c8df3dc06d6cfe66f2996f83d7f29ab40146ba
    Signed-off-by: klozz <klozz707@gmail.com>

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -100,5 +100,5 @@ FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
 ifneq ($(XPE_BUILD),)
--include $(TOPDIR)vendor/XPe/build/core/pathmap.mk
+-include $(TOPDIR)vendor/xperience/build/core/pathmap.mk
 endif
