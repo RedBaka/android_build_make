cdf3b2776544dafc63e909b892b176d6c9a779dc
klozz <klozz707@gmail.com>
2017-09-07 00:28:00 -0400

    XPerience rebranding fix a lot of issues building xpe
    Change-Id: Ib1c5df55a0847842627b02249233f82e3ac3df43
    Signed-off-by: klozz <klozz707@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,9 +179,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Lineage build needs only the Lineage product makefiles.
-ifneq ($(LINEAGE_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+# A xpe build needs only the xpe product makefiles.
+ifneq ($(XPE_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(XPE_BUILD)/xpe.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -192,9 +192,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # LINEAGE_BUILD
+endif # XPE_BUILD
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(XPE_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
